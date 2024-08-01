3eb0bc046ba9b7c15329602d061bc5fea3a75c8a
Andrés Aguilar <andresd.aguilar@gmail.com>
2024-08-01 15:18:11 -0300

    initial commit with basic structure with vertical slicing

---
+++ v1/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+exports.authMiddleware = (req, res, next) => {
+  const token = req.header('Authorization').replace('Bearer ', '');
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized access' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid Token' });
+  }
+};
